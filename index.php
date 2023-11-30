0dc7fec5f8cc5a272545c0919f553d355ca56479
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-11-30 17:00:21 +0100

    added repetition possibility

--- index.php
+++ index.php
@@ -8,14 +8,30 @@ include __DIR__ . "/partials/header.php";
     </section>
     <form action="login.php" method="GET">
         <div class="row flex-column">
-            <div class="col-12 d-flex align-items-center">
+            <div class="col-12 d-flex align-items-center py-4">
                 <label for="password" class="form-label w-25 text-center">
                     Lunghezza password:
                 </label>
                 <input type="number" class="form-control w-25" name="password_length" min="8" max="20"
                     placeholder="8-20">
             </div>
-            <div class="col-6"></div>
+            <div class="col-12 d-flex align-items-center py-4">
+                <label for="repetition" class="w-25 text-center">
+                    Consenti ripetizioni
+                </label>
+                <div class="form-check me-4">
+                    <input class="form-check-input" type="radio" name="repetition" value="si">
+                    <label class="form-check-label" for="repetition">
+                        Sì
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="repetition" value="no" checked>
+                    <label class="form-check-label" for="repetition">
+                        No
+                    </label>
+                </div>
+            </div>
         </div>
         <button class="btn btn-primary" type="submit">Genera</button>
         <button class="btn btn-secondary" type="reset">Reset</button>
--- logic/functions.php
+++ logic/functions.php
@@ -1,21 +1,38 @@
 <?php
+
 function generatePassword()
 {
     $symbols = '!?&%$<>^+-*/()[]{}@#_=';
     $allowed_letters = 'zjkvwrtypleqbihgfsax';
     $allowed_up_letters = strtoupper($allowed_letters);
     $allowed_numbers = '0123456789';
-    if (isset($_GET['password_length'])) {
+
+    if (isset($_GET['password_length']) && isset($_GET['repetition'])) {
         $password_length = $_GET['password_length'];
+        $repetition = $_GET['repetition'];
         $new_password = '';
+
         while (strlen($new_password) < $password_length) {
             $allowed_chars = $symbols . $allowed_letters . $allowed_up_letters . $allowed_numbers;
-            $char = $allowed_chars[rand(0, strlen($allowed_chars))];
-            $new_password .= $char;
+            $char = $allowed_chars[rand(0, strlen($allowed_chars) - 1)];
+
+            if (doRepeat($repetition, $char, $new_password)) {
+                $new_password .= $char;
+            }
         }
+
         $_SESSION['password'] = $new_password;
     } else {
         return false;
+    }
+}
+
+function doRepeat($repetition, $char, $password)
+{
+    if ($repetition == 'si') {
+        return true;
+    } else {
+        return strpos($password, $char) === false;
     }
 }
 ?>
